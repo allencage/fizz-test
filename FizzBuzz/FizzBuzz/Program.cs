a29c492a63deb00bd1b14bc96c3a987fc7b50dc7
allencage <allencage@allencage>
2016-04-26 14:20:49 +0200

    Finished FizzBuzz software developer test

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			var count = new FizzBuzzCounting();
+			count.Counting();
+			Console.ReadKey();
+        }
+    }
+
+    class FizzBuzzCounting
+    {
+		public void Counting()
+		{
+			for (int i = 1; i < 101; i++)
+			{
+				if ((i % 3 == 0) && (i % 5 == 0))
+					Console.WriteLine("FizzBuzz");
+				else if (i % 5 == 0)
+					Console.WriteLine("Buzz");
+				else if (i % 3 == 0)
+					Console.WriteLine("Fizz");
+				else
+					Console.WriteLine(i);
+			}
+		}
+    }
+}
